c6df95604e60d93d0bf02f7f1a21df6f39e5b1e9
Rean Griffith <rean@caa.columbia.edu>
2016-10-15 22:25:06 -0700

    Add example launch command (comment) from cli to module.py

--- wildfly9/module.py
+++ wildfly9/module.py
@@ -20,3 +20,6 @@ api.require('cli')
 #full = api.run('/cli/cli.so')
 #default = full
 default = api.run('/cli/cli.so')
+
+# To run use:
+# sudo scripts/run.py -n -v -V -e "java.so -cp .:/usr/wildfly/jboss-modules.jar:/usr/wildfly/modules/system/layers/base/org/jboss/logmanager/main/jboss-logmanager-2.0.0.Final.jar -D[Standalone] -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=org.jboss.byteman -Djava.awt.headless=true -Dorg.jboss.boot.log.file=/usr/wildfly/standalone/log/server.log -Dlogging.configuration=file:/usr/wildfly/standalone/configuration/logging.properties -Djboss.home.dir=/usr/wildfly -Djboss.server.base.dir=/usr/wildfly/standalone -Djboss.bind.address=0.0.0.0 -jar /usr/wildfly/jboss-modules.jar -mp /usr/wildfly/modules org.jboss.as.standalone"
