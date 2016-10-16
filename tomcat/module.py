14c00cac529831fe5889caaeb2b996f660f29815
Rean Griffith <rean@caa.columbia.edu>
2016-10-15 21:44:08 -0700

    Added support for tomcat 8 and tomcat 9

--- tomcat/module.py
+++ tomcat/module.py
@@ -1,5 +1,23 @@
+from osv.modules.api import *
+from osv.modules.filemap import FileMap
 from osv.modules import api
 
+require('lua')
+require('ncurses')
+require('libedit')
+require_running('httpserver')
+
+usr_files = FileMap()
+usr_files.add('${OSV_BASE}/modules/cli').to('/cli') \
+        .include('cli.so') \
+        .include('cli.lua') \
+        .include('lib/**') \
+        .include('commands/**')
+
+api.require('java')
+api.require('cli')
+
+'''
 _catalina_base = "/usr/tomcat"
 _catalina_home = _catalina_base
 _catalina_tmpdir = _catalina_base + "/temp"
@@ -12,7 +30,7 @@ _logging_config = [
 ]
 _classpath.append("%s/bin/tomcat-juli.jar" % _catalina_base)
 
-default = api.run_java(
+default_old = api.run_java(
         classpath=_classpath,
         args=_logging_config + [
             "-Dcatalina.base=%s" % _catalina_base,
@@ -20,3 +38,5 @@ default = api.run_java(
             "-Djava.io.tmpdir=%s" % _catalina_tmpdir,
             "org.apache.catalina.startup.Bootstrap", "start"
         ])
+'''
+default = api.run('/cli/cli.so')
